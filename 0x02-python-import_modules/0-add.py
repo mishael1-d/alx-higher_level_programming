047e3cadd217d798d67d57d17a25c3344ad0e997
mishael dada <mishaeldada@gmail.com>
2022-06-02 20:27:14 +0100

    fix error

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 from add_0 import add
-a = 43
-b = 5
-
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+if __name__ == '__main__':
+    a = 43
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
