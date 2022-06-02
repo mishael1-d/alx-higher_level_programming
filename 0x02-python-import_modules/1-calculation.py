c56b77a2dab9dba982714bbd108d019c2bdcc539
mishael dada <mishaeldada@gmail.com>
2022-06-02 20:43:21 +0100

    fix

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,11 +4,11 @@ from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
     import sys
 
-    
+
 a = 10
 b = 5
 
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-print("{:d} + {:d} = {:d}".format(a, b, sub(a, b)))
-print("{:d} + {:d} = {:d}".format(a, b, mul(a, b)))
-print("{:d} + {:d} = {:d}".format(a, b, div(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} + {} = {}".format(a, b, sub(a, b)))
+print("{} + {} = {}".format(a, b, mul(a, b)))
+print("{} + {} = {}".format(a, b, div(a, b)))
